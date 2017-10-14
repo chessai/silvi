63835357c28651e1a12b64ff4b1aa4522d4088bf
chessai <chessai1996@gmail.com>
2017-10-13 23:12:17 -0400

    worked a lot on build scripts

---
+++ library/Silvi.hs
@@ -0,0 +1,4 @@
+module Silvi
+ ( ) where
+
+
---
+++ nix/silvi.nix
@@ -0,0 +1,13 @@
+{ mkDerivation, base, ip, random-fu, stdenv }:
+mkDerivation {
+  pname = "silvi";
+  version = "1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ base ip random-fu ];
+  executableHaskellDepends = [ base ip random-fu ];
+  homepage = "https://github.com/chessai/silvi";
+  description = "A generator for different kinds of logs";
+  license = stdenv.lib.licenses.asl20;
+}
