9c6ef0b62451a2c3acb59a94bf519522aa37d227
chessai <chessai1996@gmail.com>
2017-10-20 14:19:39 -0400

    unify with other projects

--- repl.sh
+++ repl.sh
@@ -5,12 +5,16 @@
 rm -rf dist
 
 ## jailbreaks
+# chronos
+cabal2nix https://github.com/chessai/chronos.git > nix/chronos.nix
 # http-types
 cabal2nix https://github.com/chessai/http-types.git > nix/http-types.nix
 # savage
 cabal2nix https://github.com/chessai/savage.git > nix/savage.nix
-# use version of chronos with some needed features
-cabal2nix https://github.com/chessai/chronos.git > nix/chronos.nix
 
 #this is a workaround while cabal-repl is broken (https://github.com/haskell/cabal/issues/4602)
 nix-shell -A silvi.env release.nix --run "runhaskell Setup.hs configure; runhaskell Setup.hs repl lib:silvi"
+
+# dist can cause problems with nix-build (called by nix-shell)
+# so delete this in between builds
+rm -rf dist
